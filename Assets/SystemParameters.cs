cd1ba53bddb032ef6ee30bd5bf9d8dec30816132
mut <gustavo.ceci95@gmail.com>
2019-01-26 16:01:49 -0800

    added body rectangle parameters do system parameters

--- Assets/SystemParameters.cs
+++ Assets/SystemParameters.cs
@@ -6,9 +6,12 @@ public class SystemParameters : MonoBehaviour
 {
   private static SystemParameters Instance;
   public static CellSystemParameters CellSystem => Instance._cellSystemParameters;
-
   [SerializeField]
   private CellSystemParameters _cellSystemParameters;
+
+  public static BodyRectangleSystemParameters BodyRectangleSystem => Instance._bodyRectangleSystemParameters;
+  [SerializeField]
+  private BodyRectangleSystemParameters _bodyRectangleSystemParameters;
   void Awake()
   {
     var instance = FindObjectOfType<SystemParameters>();
