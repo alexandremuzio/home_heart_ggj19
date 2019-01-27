fef4cc948057fb3e6888863177f8e0bd586d6cf4
Alexandre Muzio <ax.muzio@gmail.com>
2019-01-26 16:06:56 -0800

    Creating some systems and components

---
+++ Assets/Systems/BodyRectangleSystem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Burst;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Collections;
+using Unity.Rendering;
+
+public class BodyRectangleSystem : ComponentSystem
+{
+    struct DecreaseOxygenComponents
+    {
+        public Position position;
+        public BodyRectangleComponent bodyRect;
+    }
+
+    struct ChangeColorOxygenComponents
+    {
+        public SpriteRenderer renderer;
+        public BodyRectangleComponent body;
+    }
+
+    protected override void OnUpdate()
+    {
+        foreach (var e in GetEntities<DecreaseOxygenComponents>()){ 
+            e.bodyRect.OxygenLevel -= Time.deltaTime * e.bodyRect.OxygenDecreaseSpeed;
+        }
+
+        foreach (var e in GetEntities<ChangeColorOxygenComponents>()) {
+            e.renderer.col
+        }
+    }
+}    
