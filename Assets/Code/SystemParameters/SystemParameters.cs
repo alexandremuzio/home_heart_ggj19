11c6b04e6bdc28e2cad8599077d4bda8ac7551fa
mut <gustavo.ceci95@gmail.com>
2019-01-26 16:24:35 -0800

    better singleton for system parameters

---
+++ Assets/Code/SystemParameters/SystemParameters.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemParameters : MonoBehaviour
+{
+  public static CellSystemParameters CellSystem => Instance._cellSystemParameters;
+  [SerializeField]
+  private CellSystemParameters _cellSystemParameters;
+
+  public static BodyRectangleSystemParameters BodyRectangleSystem => Instance._bodyRectangleSystemParameters;
+  [SerializeField]
+  private BodyRectangleSystemParameters _bodyRectangleSystemParameters;
+  private static SystemParameters _instance;
+  private static SystemParameters Instance
+  {
+    get
+    {
+      var instance = FindObjectOfType<SystemParameters>();
+      if (instance != Instance)
+      {
+        Destroy(instance);
+        return _instance;
+      }
+      _instance = instance;
+      return _instance;
+    }
+  }
+}
