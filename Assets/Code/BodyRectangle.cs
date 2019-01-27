2aab3ed88b0cdb15c231378b61eab99b11abf1ef
mut <gustavo.ceci95@gmail.com>
2019-01-26 23:37:45 -0800

    basic character and some more stuff

--- Assets/Code/BodyRectangle.cs
+++ Assets/Code/BodyRectangle.cs
@@ -4,43 +4,45 @@ using UnityEngine;
 
 public enum BodySplitType
 {
-    Vertical,
-    Horizontal,
-    Cross,
+  Vertical,
+  Horizontal,
+  Cross,
 };
 
 [RequireComponent(typeof(SpriteRenderer))]
-public class BodyRectangle : MonoBehaviour 
+public class BodyRectangle : MonoBehaviour
 {
-    public float oxgyenLevel;
-    public float oxygenReduceSpeed;
-    public bool isAlive;
+  public float oxgyenLevel;
+  public float oxygenReduceSpeed;
+  public bool isAlive;
 
-    private new SpriteRenderer renderer;
+  private new SpriteRenderer renderer;
 
-    public Vector2 position => gameObject.transform.position;
-    public float HorizontalSize => renderer.size.x;
-    public float VerticalSize => renderer.size.y;
+  public Vector2 position => gameObject.transform.position;
+  public float HorizontalSize => renderer.size.x;
+  public float VerticalSize => renderer.size.y;
 
-    public void Initialize(float horizontalSize, float verticalSize) {
-        // sets rect size
-        Vector2 size = new Vector2(horizontalSize, verticalSize);
+  public void Initialize(float horizontalSize, float verticalSize)
+  {
+    // sets rect size
+    Vector2 size = new Vector2(horizontalSize, verticalSize);
 
-        renderer.size = size;
-    }
+    renderer.size = size;
+  }
 
-    private void Awake()
+  private void Awake()
+  {
+    renderer = GetComponent<SpriteRenderer>();
+  }
+
+  private void Update()
+  {
+    this.oxgyenLevel -= Time.deltaTime * oxygenReduceSpeed;
+
+    if (this.oxgyenLevel < 0f)
     {
-        renderer = GetComponent<SpriteRenderer>();
+      isAlive = false;
+      //print("Body Rectangle is dead!");
     }
-
-    private void Update()
-    {
-        this.oxgyenLevel -= Time.deltaTime * oxygenReduceSpeed;
-
-        if (this.oxgyenLevel < 0f) {
-            isAlive = false;
-            print("Body Rectangle is dead!");
-        }
-    }
+  }
 }
