ebb0ef6c2665c9059878c3179227cf5b5c069d8a
Alexandre Muzio <ax.muzio@gmail.com>
2019-02-01 08:03:56 -0800

    Cleaning rubbish 🚮

--- Assets/Code/BloodCellSystem/BloodCellOxigenation.cs
+++ Assets/Code/BloodCellSystem/BloodCellOxigenation.cs
@@ -6,49 +6,47 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class BloodCellOxigenation : MonoBehaviour
 {
-  public BloodCellData data;
-  public SpriteRenderer spriterenderer;
+    public BloodCellData data;
+    public SpriteRenderer spriterenderer;
 
-  private OxigenationMeter oxigenationMeter;
+    private OxigenationMeter oxigenationMeter;
 
-  private void Awake()
-  {
-    oxigenationMeter = GetComponent<OxigenationMeter>();
-    spriterenderer = GetComponent<SpriteRenderer>();
-  }
-
-  private void Update()
-  {
-    spriterenderer.color = oxigenationMeter.GetColor();
-  }
-
-  private void OnTriggerStay2D(Collider2D other)
-  {
-    var rect = other.gameObject.GetComponent<BodyRectangle>();
-    if (rect)
+    private void Awake()
     {
-      var rectMeter = rect.GetComponent<OxigenationMeter>();
-      if (!rectMeter.IsAlive) return;
-      var fillAmmount = data.OxigenDonationRate * Time.deltaTime;
-
-      Func<float, float> take = oxigenationMeter.Take;
-
-      fillAmmount
-      .Forward(oxigenationMeter.Take)
-      .Forward(rectMeter.Give)
-      .Forward(oxigenationMeter.Give);
+        oxigenationMeter = GetComponent<OxigenationMeter>();
+        spriterenderer = GetComponent<SpriteRenderer>();
     }
 
-    var home = other.gameObject.GetComponent<BloodCellHome>();
-    if (home)
+    private void Update()
     {
-      oxigenationMeter.Give(oxigenationMeter.maxOxygenLevel);
+        spriterenderer.color = oxigenationMeter.GetColor();
     }
-  }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        var rect = other.gameObject.GetComponent<BodyRectangle>();
+        if (rect)
+        {
+            var rectMeter = rect.GetComponent<OxigenationMeter>();
+            if (!rectMeter.IsAlive) return;
+            var fillAmmount = data.OxigenDonationRate * Time.deltaTime;
+
+            fillAmmount
+            .Forward(oxigenationMeter.Take)
+            .Forward(rectMeter.Give)
+            .Forward(oxigenationMeter.Give);
+        }
+
+        var home = other.gameObject.GetComponent<BloodCellHome>();
+        if (home)
+        {
+            oxigenationMeter.Give(oxigenationMeter.maxOxygenLevel);
+        }
+    }
 }
 
 public static partial class FuncExtensions
 {
-  public static TResult Forward<T, TResult>(this T value, Func<T, TResult> function) =>
-      function(value);
+    public static TResult Forward<T, TResult>(this T value, Func<T, TResult> function) =>
+        function(value);
 }
